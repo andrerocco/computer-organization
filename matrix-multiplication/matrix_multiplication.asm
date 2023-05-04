f54a62b6ed1c20837f9f110f91d8b8be7290433f
André Amaral Rocco <andre.amaral.rocco@gmail.com>
2023-05-04 11:09:46 -0300

    Iniciado exercício

---
+++ matrix-multiplication/matrix_multiplication.asm
@@ -0,0 +1,11 @@
+# Dadas as matrizes A e B (abaixo), elabore um programa em Assembly para o processador MIPS (usando o
+# simulador MARS) que encontre a matriz resultante do produto: A.B. Armazene a matriz resultante na
+# memória de dados.
+
+# | 1  2  3 |     | 1 -2  5 |
+# | 0  1  4 |  x  | 0  1 -4 |
+# | 0  0  1 |     | 0  0  1 |
+
+.data
+
+.text
